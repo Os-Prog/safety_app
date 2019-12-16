c98628de66909b58bfa28dd7ae702de237aee5c0
os-prog <josnagato@gmail.com>
2019-12-16 20:57:56 +0100

    latest update

---
+++ lib/screens/e_loading.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingR extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: RetClass(),
+    );
+  }
+}
+
+class RetClass extends StatefulWidget {
+  @override
+  _RetClassState createState() => _RetClassState();
+}
+
+class _RetClassState extends State<RetClass> {
+  @override
+  void initState() {
+    super.initState();
+
+    sendrt();
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: CircularProgressIndicator(
+        ),
+      ),
+    );
+  }
+  void sendsignal() async{
+    Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+  void sendrt() async {
+//    sendsignal();
+    Position $position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    String $hh = $position.toString();
+
+    final url = 'http://www.ismnkwara.org/nhack/sms.php';
+    Map data = {'apikey': '12345678901234567890', 'msg': "Fire outbreak at " +$hh+" Name: Muhammed Yayah", 'num1': '08162875010', 'num2': '08146367757', 'num3': '08164093351'};
+    final body = json.encode(data);
+    var response = await http.post(url,
+        headers: {"Content-Type": "application/json"}, body: body);
+    if (response.statusCode == 200) {
+      print('hello');
+      Navigator.pop(context);
+    }else{
+      print("error");
+      Navigator.pop(context);
+    }
+  }
+
+}
+
+
