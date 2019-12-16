0ebebf9c62b7ad6462f4adf63fd4ea2274d8b956
os-prog <josnagato@gmail.com>
2019-12-16 20:54:22 +0100

    last update

--- lib/screens/emergency_screen.dart
+++ lib/screens/emergency_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'e_loading.dart';
+import 'e_loading_general.dart';
+import 'e_loading_med.dart';
+import 'e_loading_post.dart';
 
 class EmergencyScreen extends StatefulWidget {
   static final id = 'Emergency';
@@ -9,10 +15,241 @@ class EmergencyScreen extends StatefulWidget {
 class _EmergencyScreenState extends State<EmergencyScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Emergency Page'),
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(216, 216, 216, 1),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 25.0,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _sendFire();
+                          });
+                        },
+                        child: Card(
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 14.0),
+                              child: ListTile(
+                                title: Center(
+                                  child: Column(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.whatshot,
+                                        size: 52.0,
+                                        color: Colors.orangeAccent,
+                                      ),
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Text(
+                                        'Fire',
+                                        style: TextStyle(
+                                            fontSize: 18.0, fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            )
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _sendMed();
+                          });
+                        },
+                        child: Card(
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 14.0),
+                              child: ListTile(
+                                title: Center(
+                                  child: Column(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.add_box,
+                                        size: 52.0,
+                                        color: Colors.redAccent,
+                                      ),
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Text(
+                                        'Medical',
+                                        style: TextStyle(
+                                            fontSize: 18.0, fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            )
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 1.0,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _sendPolice();
+                          });
+                        },
+                        child: Card(
+                          child: Container(
+                            padding: EdgeInsets.symmetric(vertical: 14.0),
+                            child: ListTile(
+                              title: Center(
+                                child: Column(
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.directions_car,
+                                      size: 52.0,
+                                      color: Colors.blueAccent,
+                                    ),
+                                    SizedBox(
+                                      height: 10.0,
+                                    ),
+                                    Text(
+                                      'Police',
+                                      style: TextStyle(
+                                          fontSize: 18.0, fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          )
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Expanded(
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Card(
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 14.0),
+                              child: ListTile(
+                                title: Center(
+                                  child: Column(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.announcement,
+                                        size: 52.0,
+                                        color: Colors.orangeAccent,
+                                      ),
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Text(
+                                        'General',
+                                        style: TextStyle(
+                                            fontSize: 18.0, fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            )
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 1.0, vertical: 4.0),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    child: Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _sendGeneral();
+                          });
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 17.0),
+                          margin: EdgeInsets.symmetric(horizontal: 8.0),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(6.0),
+                            boxShadow: [BoxShadow(color: Colors.black38)],
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Icon(
+                                Icons.devices,
+                                size: 52.0,
+                                color: Colors.orangeAccent,
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Text(
+                                'Activate Device',
+                                style: TextStyle(
+                                    fontSize: 18.0, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
+
+  void _sendFire() async {
+    Future resp = Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LoadingR()));
+  }
+  void _sendMed() async {
+    Future resp = Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LoadingR_med()));
+  }
+  void _sendPolice() async {
+    Future resp = Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LoadingR_Police()));
+  }
+  void _sendGeneral() async {
+    Future resp = Navigator.push(
+        context, MaterialPageRoute(builder: (context) => LoadingR_Gen()));
+  }
+
 }
--- lib/screens/manage_screen.dart
+++ lib/screens/manage_screen.dart
@@ -9,9 +9,208 @@ class ManageScreen extends StatefulWidget {
 class _ManageScreenState extends State<ManageScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Manage Account'),
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(216, 216, 216, 1),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Container(
+                    color: Colors.deepPurpleAccent,
+                    height: MediaQuery.of(context).size.height * .25,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              padding: new EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * .1),
+//              height: 143.0,
+              margin: EdgeInsets.only(top: 35.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.only(left: 10.0),
+                      height: 100.0,
+                      child: Image.asset('assets/images/new-img.png'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 10.0, left: 15.0, right: 15.0),
+                    child: Container(
+                      height: 86.0,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.0)
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(top: 16.0),
+                            child: Text(
+                              'Muhammed Yayah',
+                              style: TextStyle(
+                                fontSize: 19.0,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Fullstack web developer & App developer',
+                              style: TextStyle(
+                                fontSize: 11.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 10.0, left: 15.0, right: 15.0),
+                    child: Container(
+                        padding: EdgeInsets.only(bottom: 15.0),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8.0)
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 12.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Column(
+                                    children: <Widget>[
+                                      Container(
+                                        margin: EdgeInsets.only(right: 14.0),
+                                        child: Icon(Icons.email, color: Color.fromRGBO(81, 153, 228, 1), size: 24.0,),
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    children: <Widget>[
+                                      Text('Email', style: TextStyle(fontWeight: FontWeight.bold),),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 48.0, right: 8.0, top: 4.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Text(
+                                    'examples@mail.com'
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                            SizedBox(
+                              height: 9.0,
+                            ),
+
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 12.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Column(
+                                    children: <Widget>[
+                                      Container(
+                                        margin: EdgeInsets.only(right: 14.0),
+                                        child: Icon(Icons.phone, color: Color.fromRGBO(81, 153, 228, 1), size: 24.0,),
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    children: <Widget>[
+                                      Text('Phone Number', style: TextStyle(fontWeight: FontWeight.bold),),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 48.0, right: 8.0, top: 4.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Text(
+                                      '07012345678'
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                            SizedBox(
+                              height: 9.0,
+                            ),
+
+                            Padding(
+                              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 12.0),
+                              child: Row(
+                                children: <Widget>[
+                                  Column(
+                                    children: <Widget>[
+                                      Container(
+                                        margin: EdgeInsets.only(right: 14.0),
+                                        child: Icon(Icons.contact_phone, color: Color.fromRGBO(81, 153, 228, 1), size: 24.0,),
+                                      ),
+                                    ],
+                                  ),
+                                  Column(
+                                    children: <Widget>[
+                                      Text('Contacts', style: TextStyle(fontWeight: FontWeight.bold),),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 48.0, right: 8.0, top: 4.0),
+                              child: Column(
+                                children: <Widget>[
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                          '08162875010'
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                          '08164093351'
+                                      ),
+                                    ],
+                                  ),
+                                  Row(
+                                    children: <Widget>[
+                                      Text(
+                                          '08146367757'
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
